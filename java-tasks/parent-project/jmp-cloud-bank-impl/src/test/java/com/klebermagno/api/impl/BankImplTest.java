6b1d085a18659912030cdac75c327befae31ea6b
Kleber Magno <klebermagno@gmail.com>
2022-11-25 18:49:22 +0000

    Update to JUnit 5

--- java-tasks/parent-project/jmp-cloud-bank-impl/src/test/java/com/klebermagno/api/impl/BankImplTest.java
+++ java-tasks/parent-project/jmp-cloud-bank-impl/src/test/java/com/klebermagno/api/impl/BankImplTest.java
@@ -1,12 +1,11 @@
 package com.klebermagno.api.impl;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
-import com.klebermagno.api.*;
 import com.klebermagno.dto.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 
 public class BankImplTest {
     
