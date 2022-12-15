3fd963e9016f78575d4220e2df6fc38533422df1
Kleber Magno <klebermagno@gmail.com>
2022-12-15 13:13:25 +0000

    Add new project algorithms

---
+++ efective-java/algorithms/src/test/java/com/klebermagno/algorithms/AppTest.java
@@ -0,0 +1,21 @@
+package com.klebermagno.algorithms;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void shouldBeTrue()
+    {
+
+        assertTrue(true);
+
+    }   
+}
+
